038778069da28f017db550c32adb1e98003b48c9
kkashyap123 <32977442+kkashyap123@users.noreply.github.com>
2019-04-19 21:48:40 +0530

    fields for component.ts

---
+++ src/app/create/fields.js
@@ -0,0 +1,20 @@
+fields_challan = ['challanId', 'officerId', 'licenceId',
+   'fineAmount', 'registrationNumber', 'offences'];
+fields_emergency = ['streetId', 'officerId', 'typeOfEmergency'];
+fields_crossing=['crossingId', 'crossingName', 'typesOfCrossing','connectingStreetId','officerId'];
+fields_licence = ['licenceId', 'name', 'authorization', 'dateOfBirth', 'dateOfIssue', 'expirationDate', 'address', 'bloodGroup'];
+field_policeOfficer= ['offierId', 'age', 'name', 'rank', 'bloodGroup', 'StationId'];
+field_policeStation=['policeStation', 'address', 'policeStationName', ];
+field_rto=['rtoCode','address','head'];
+field_street=['streetId','streetName','numberofLanes','typeOfRoad'];
+field_street=['streetId','streetName','numberofLanes','typeOfRoad'];
+field_trafficLight=['trafficLightId', 'crossingId'];
+
+
+
+
+
+
+
+
+
